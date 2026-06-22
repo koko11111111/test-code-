b61d73fc34b4ded751e1c8ab2b550314b5852cb5
koko11111111 <kokomina946@gmail.com>
2026-06-22 19:38:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js"></script>
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyDklnZRCFnxxHLP_MV6j4_ZR5vykY1LQ0E",
   authDomain: "relay-8a807.firebaseapp.com",
   projectId: "relay-8a807",
@@ -10,7 +7,3 @@ const firebaseConfig = {
   appId: "1:219719482946:web:967caac406eb31131df7db",
   measurementId: "G-B5WEX4SY8S"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
